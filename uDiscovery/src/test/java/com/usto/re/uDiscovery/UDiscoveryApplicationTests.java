46b90e272abf54245b5dd9d0418aba658141f9a5
Davi Fernandes Alves da Silva <57471802+Davizex@users.noreply.github.com>
2024-06-11 22:14:26 -0300

    Add files via upload

---
+++ uDiscovery/src/test/java/com/usto/re/uDiscovery/UDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usto.re.uDiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
